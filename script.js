dda00ea6cb165bbe03cfa3edaee30995598994ea
timoWantsToBeADev <99825941+timoWantsToBeADev@users.noreply.github.com>
2024-02-13 22:41:38 +0100

    added bg draw function

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
+const scoreEl = document.getElementById("score");
+const caloriesEl = document.getElementById("calories");
+const scorelineEl = document.getElementById("scoreline");
+const startbtnEl = document.getElementById("start-btn");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-const blueyColor = "#00a0ce";
-let a = 0;
-const gravity = 0.7;
-function runGame() {
-  animate();
-}
-
-
-
-
 canvas.width = 1024;
 canvas.height = 576;
 
+const blueyColor = "#00a0ce";
+const gravity = 0.7;
+const maxJump = 3;
+let points = 0;
+let calories = 0;
+
+
+
+const randomX = () => { return Math.floor(Math.random() * (canvas.width + 1)) }
+const randomY = () => { return (Math.floor(Math.random() * 350) + 50) }
+const randomW = () => { return (Math.floor(Math.random() * 10) + 10) }
+const randomH = () => { return (Math.floor(Math.random() * 10) + 10) }
+
+
+
+function runGame() {
+  animate();
+  startbtnEl.hidden = true;
+}
+
+
+// drawing the canvas
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+
 
 
 class Sprite {
@@ -23,7 +41,8 @@ class Sprite {
     this.velocity = velocity;
     this.height = height;
     this.width = width;
-    this.lastkey
+    this.jump = false;
+    this.jumpcount = 0;
   }
 
   draw() {
@@ -31,31 +50,50 @@ class Sprite {
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
   update() {
-
     if (this.position.y + this.height + this.velocity.y >= canvas.height) {
       this.velocity.y = 0;
+      this.jumpcount = 0;
+    }
+
+    if (this.position.x < 0) {
+      this.position.x = 0;
+    } else if (this.position.x + this.width > 1024) {
+      this.position.x = 1024 - this.width;
+    } else if (this.position.y < 0) {
+      this.position.y = 0;
+    }
+
+    bluey.velocity.x = 0;
+
+    if (liveKeys[0] === "a") { this.velocity.x = -3 }
+    else if (liveKeys[0] === "d") { this.velocity.x = 3 }
+
+    if (this.jump === true && this.jumpcount < maxJump && bluey.position.y > 30) {
+      this.velocity.y = -12;
+      //////////<---- jump timer in update!
 
     }
 
     this.draw();
     this.position.y += this.velocity.y;
     this.position.x += this.velocity.x;
-    // console.log(`lastkey is ${this.lastkey}   A pressed state is ${keys.a.pressed}, D pressed state is ${keys.d.pressed}`)
   }
 }
 
 
 class Cheese {
-  constructor({ position, size, duration }) {
+  constructor({ position, duration, width, height }) {
     this.position = position
-    this.size = size
+    this.height = height
+    this.width = width
     this.duration = duration
   }
 
   draw() {
     ctx.fillStyle = "yellow";
-    ctx.fillRect(this.position.x, this.position.y, this.size.width, this.size.height);
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
+
 
 }
 
@@ -73,97 +111,103 @@ const bluey = new Sprite({
 });
 
 
-const kaas = new Cheese({
-  position: { x: 800, y: 300 },
-  size: { width: 10, height: 10 },
+
+let kaas = new Cheese({
+  position: { x: randomX(), y: randomY() },
+  width: 10,
+  height: 10,
   duration: 5
 })
 
 function animate() {
   window.requestAnimationFrame(animate);
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  // ctx.fillStyle = "black";
+  // ctx.fillRect(0, 0, canvas.width, canvas.height);
+  drawBG();
   bluey.velocity.y += gravity;
   bluey.update();
   kaas.draw();
-
-
-
-  bluey.velocity.x = 0;
-  if (keys.a.pressed === true && bluey.lastkey === "a") { bluey.velocity.x = -3 }
-  if (keys.d.pressed === true && bluey.lastkey === "d") { bluey.velocity.x = 3 }
-
-
+  checkCol();
 }
 
-
-const keys = {
-  w: {
-    pressed: false
-  },
-  a: {
-    pressed: false
-  },
-  d: {
-    pressed: false
-  },
-  s: {
-    pressed: false
-  }
-}
-
-
-
-
+let liveKeys = []; //liveKeys[0] is leading direction
 
 window.addEventListener("keydown", (event) => {
-  console.log(`key down = ${event.key}`)
+  if (!liveKeys.includes(event.key) && (event.key === "a" || event.key === "d")) {
+    liveKeys.unshift(event.key);
+  }
+});
 
-  switch (event.key) {
-    case "w": bluey.velocity.y = -15;
-      //console.log(keys.ArrowUp.pressed); break;
-      break;
-    case "a": keys.a.pressed = true;
-      bluey.lastkey = "a";
-      break;
-    //console.log(keys.ArrowUp.pressed); break;
 
-    case "d": keys.d.pressed = true;
-      bluey.lastkey = "d";
-      break;
-    //console.log(keys.ArrowUp.pressed); break;
 
-    case "s": keys.s.pressed = true;
-      break;
-    //console.log(keys.ArrowUp.pressed); break;
+///separate jump eventlistener
+window.addEventListener("keydown", (event) => {
+  if (event.key === "w" && bluey.jumpcount < maxJump && !bluey.jump) {
+    bluey.jump = true;
+    bluey.jumpcount++;
+    setTimeout(() => {
+      bluey.jump = false;
+    }, 100);
 
-    default:
-      break;
+
   }
 
 });
 
 
 window.addEventListener("keyup", (event) => {
-  //console.log(`key up = ${event.key}`)
+  if (liveKeys.includes(event.key) && (event.key === "a" || event.key === "d")) {
+    liveKeys = liveKeys.filter((key) => key != event.key)
 
-  switch (event.key) {
-    case "w": keys.w.pressed = false;
-    //console.log(keys.ArrowUp.pressed)
-
-    case "a": keys.a.pressed = false;
-    //console.log(keys.ArrowUp.pressed); break;
-
-    case "d": keys.d.pressed = false;
-    //console.log(keys.ArrowUp.pressed); break;
-
-    case "s": keys.s.pressed = false;
-    //console.log(keys.ArrowUp.pressed); break;
-
-
-    default:
-      break;
   }
 
+  if (event.key === "w") {
+    bluey.jump = false;
+  }
 });
+
+
+function checkCol() {
+  if (bluey.position.x + bluey.width >= kaas.position.x && bluey.position.x <= kaas.position.x + kaas.width &&
+    bluey.position.y <= kaas.position.y + kaas.height && bluey.position.y + bluey.height >= kaas.position.y) {
+
+    kaas = new Cheese({
+      position: { x: randomX(), y: randomY() },
+      width: randomW(),
+      height: randomH(),
+      duration: 5
+    })
+    bluey.width += 0.2;
+    bluey.height += 0.2;
+    points++
+    calories += (kaas.height * kaas.width) / 2
+    if(scorelineEl.hidden) {
+      scorelineEl.hidden = false
+    }
+
+
+    scoreEl.textContent = points;
+    caloriesEl.textContent = Math.floor(calories);
+    console.log("Yummmmmmy")
+
+  }
+}
+
+
+function drawBG(){
+    bg = new Image(canvas.width,canvas.height)
+    bg.src = "./img/background.png";
+    ctx.drawImage(
+      bg,
+      0,
+      0,
+      bg.width,
+      bg.height
+    )
+}
+
+
+
+
+
 
