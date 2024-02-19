6b47567a358bb8d0807d0f472096fed46eddab6d
timoWantsToBeADev <99825941+timoWantsToBeADev@users.noreply.github.com>
2024-02-19 23:33:25 +0100

    added lifespan lives totalscore after

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const scoreEl = document.getElementById("score");
-const caloriesEl = document.getElementById("calories");
+const livesEl = document.getElementById("lives");
 const scorelineEl = document.getElementById("scoreline");
 const startbtnEl = document.getElementById("start-btn");
 const canvas = document.querySelector("canvas");
@@ -17,15 +17,24 @@ const gravity = 0.7;
 const maxJump = 3;
 let points = 0;
 let calories = 0;
+let duration = 7;
+let timeLeft = duration;
+let lives = 3;
+let gameRunning = true;
+let speedModifier = 1;
 
-
-
-const randomX = () => { return Math.floor(Math.random() * (canvas.width + 1)) }
-const randomY = () => { return (Math.floor(Math.random() * 350) + 50) }
-const randomW = () => { return (Math.floor(Math.random() * 10) + 10) }
-const randomH = () => { return (Math.floor(Math.random() * 10) + 10) }
-
-
+const randomX = () => {
+  return Math.floor(Math.random() * (canvas.width - 20 + 1));
+};
+const randomY = () => {
+  return Math.floor(Math.random() * 350) + 50;
+};
+const randomW = () => {
+  return Math.floor(Math.random() * 10) + 10;
+};
+const randomH = () => {
+  return Math.floor(Math.random() * 10) + 10;
+};
 
 function runGame() {
   btnAudio.play();
@@ -34,15 +43,19 @@ function runGame() {
   soundtrackAudio.play();
   animate();
   startbtnEl.hidden = true;
-}
+  gameRunning = true;
 
+  const speedUp = setInterval(() => {
+    if (duration > 3) {
+      duration -= 1;
+      console.log(`duration ${duration}`);
+    } else clearInterval(speedUp);
+  }, 15000);
+}
 
 // drawing the canvas
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-
-
 
 // class Sprite {
 //   constructor({ position, velocity, height, width }) {
@@ -89,7 +102,6 @@ ctx.fillRect(0, 0, canvas.width, canvas.height);
 //   }
 // }
 
-
 class Charachter {
   constructor({ position, velocity, height, width }) {
     this.position = position;
@@ -98,10 +110,8 @@ class Charachter {
     this.width = width;
     this.jump = false;
     this.jumpcount = 0;
-
-    this.image = new Image(this.width, this.height)
+    this.image = new Image(this.width, this.height);
     this.image.src = "./img/bird.png";
-
   }
 
   draw() {
@@ -134,17 +144,18 @@ class Charachter {
 
     if (liveKeys[0] === "a") {
       this.velocity.x = -3;
-      this.image.src = "./img/birdLeft.png"
-    }
-    else if (liveKeys[0] === "d") {
+      this.image.src = "./img/birdLeft.png";
+    } else if (liveKeys[0] === "d") {
       this.velocity.x = 3;
-      this.image.src = "./img/bird.png"
+      this.image.src = "./img/bird.png";
     }
 
-    if (this.jump === true && this.jumpcount < maxJump && this.position.y > 30) {
+    if (
+      this.jump === true &&
+      this.jumpcount < maxJump &&
+      this.position.y > 30
+    ) {
       this.velocity.y = -12;
-
-
     }
 
     this.draw();
@@ -153,31 +164,24 @@ class Charachter {
   }
 }
 
-
-
-
 class Cheese {
-  constructor({ position, duration, width, height }) {
-    this.position = position
-    this.height = height
-    this.width = width
-    this.duration = duration
-    this.image = new Image(this.width, this.height)
-    this.image.src = "./img/kaas.png"
+  constructor({ position, width, height }) {
+    this.position = position;
+    this.height = height;
+    this.width = width;
+    this.image = new Image(this.width, this.height);
+    this.image.src = "./img/kaas.png";
+    this.id = crypto.randomUUID();
   }
 
   draw() {
-    ctx.drawImage(
-      this.image,
-      this.position.x,
-      this.position.y,
-      30,
-      30
-    )
-
+    ctx.drawImage(this.image, this.position.x, this.position.y, 30, 30);
   }
 
-
+  update() {
+    this.draw();
+    countDown();
+  }
 }
 
 const bluey = new Charachter({
@@ -190,40 +194,40 @@ const bluey = new Charachter({
     y: 0,
   },
   height: 50,
-  width: 50
+  width: 50,
 });
-
-
 
 let kaas = new Cheese({
   position: { x: randomX(), y: randomY() },
   width: 10,
   height: 10,
-  duration: 5
-})
+});
 
 function animate() {
-  setTimeout(() => {
-
-    window.requestAnimationFrame(animate);
-    drawBG();
-    bluey.velocity.y += gravity;
-    bluey.update();
-    kaas.draw();
-    checkCol();
-
-  }, 1000 / fps);
+  if (lives > 0) {
+    setTimeout(() => {
+      window.requestAnimationFrame(animate);
+      drawBG();
+      bluey.velocity.y += gravity;
+      bluey.update();
+      kaas.update();
+      checkCol();
+    }, 1000 / fps);
+  } else {
+    gameOVer(points);
+  }
 }
 
 let liveKeys = []; //liveKeys[0] is leading direction
 
 window.addEventListener("keydown", (event) => {
-  if (!liveKeys.includes(event.key) && (event.key === "a" || event.key === "d")) {
+  if (
+    !liveKeys.includes(event.key) &&
+    (event.key === "a" || event.key === "d")
+  ) {
     liveKeys.unshift(event.key);
   }
 });
-
-
 
 ///separate jump eventlistener
 window.addEventListener("keydown", (event) => {
@@ -233,17 +237,15 @@ window.addEventListener("keydown", (event) => {
     setTimeout(() => {
       bluey.jump = false;
     }, 100);
-
-
   }
-
 });
 
-
 window.addEventListener("keyup", (event) => {
-  if (liveKeys.includes(event.key) && (event.key === "a" || event.key === "d")) {
-    liveKeys = liveKeys.filter((key) => key != event.key)
-
+  if (
+    liveKeys.includes(event.key) &&
+    (event.key === "a" || event.key === "d")
+  ) {
+    liveKeys = liveKeys.filter((key) => key != event.key);
   }
 
   if (event.key === "w") {
@@ -251,44 +253,59 @@ window.addEventListener("keyup", (event) => {
   }
 });
 
-
 function checkCol() {
-  if (bluey.position.x + bluey.width >= kaas.position.x && bluey.position.x <= kaas.position.x + kaas.width &&
-    bluey.position.y <= kaas.position.y + kaas.height && bluey.position.y + bluey.height >= kaas.position.y) {
+  if (
+    bluey.position.x + bluey.width >= kaas.position.x &&
+    bluey.position.x <= kaas.position.x + kaas.width &&
+    bluey.position.y <= kaas.position.y + kaas.height &&
+    bluey.position.y + bluey.height >= kaas.position.y
+  ) {
     eatAudio.play();
     kaas = new Cheese({
       position: { x: randomX(), y: randomY() },
       width: randomW(),
       height: randomH(),
-      duration: 5
-    })
+    });
     bluey.width += 0.2;
     bluey.height += 0.2;
-    points++
-    calories += (kaas.height * kaas.width) / 2
+    points++;
+    calories += (kaas.height * kaas.width) / 2;
     if (scorelineEl.hidden) {
-      scorelineEl.hidden = false
+      scorelineEl.hidden = false;
     }
 
-
     scoreEl.textContent = points;
-    caloriesEl.textContent = Math.floor(calories);
-    console.log("Yummmmmmy")
-
+    livesEl.textContent = lives;
+    bluey.jumpcount--;
   }
 }
 
-
 function drawBG() {
-  bg = new Image(canvas.width, canvas.height)
+  bg = new Image(canvas.width, canvas.height);
   bg.src = "./img/background.png";
-  ctx.drawImage(
-    bg,
-    0,
-    0,
-    bg.width,
-    bg.height
-  )
+  ctx.drawImage(bg, 0, 0, bg.width, bg.height);
 }
 
+function countDown() {
+  if (!kaas.counting) {
+    kaas.counting = true;
+    let countingId = kaas.id;
+    setTimeout(() => {
+      if (countingId === kaas.id) {
+        kaas = new Cheese({
+          position: { x: randomX(), y: randomY() },
+          width: 10,
+          height: 10,
+        });
+        lives--;
+        livesEl.textContent = lives;
+        console.log(kaas.id);
+      }
+    }, 1000 * duration);
+  }
+}
 
+function gameOVer(points) {
+  window.alert(`Game over, your score was ${points}`);
+  location.reload();
+}
